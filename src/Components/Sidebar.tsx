abcddc6cfd2d7fa0dff39218595dfeed603827f9
nedakinduryte <neda.kinduryte@gmail.com>
2019-11-06 07:09:19 +0000

    Changed useStyles to typescript

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -3,26 +3,28 @@ import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
 import moment from 'moment';
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: 200,
-    },
-    button: {
-        margin: theme.spacing(1),
-    },
-    input: {
-        display: 'none',
-    }
-}))
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            width: 200,
+        },
+        button: {
+            margin: theme.spacing(1),
+        },
+        input: {
+            display: 'none',
+        }
+    })
+)
 
 interface ComponentProps {
     getTargetValues(date: string, eventName: string): void;
