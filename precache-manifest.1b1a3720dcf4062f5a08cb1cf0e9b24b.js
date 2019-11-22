c50fc4c93d452d0ecb40bf6a23a102665252f59a
nedakinduryte <neda.kinduryte@gmail.com>
2019-11-22 08:09:39 +0000

    Updates

---
+++ precache-manifest.1b1a3720dcf4062f5a08cb1cf0e9b24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887428e47d6e0b4d89f21bdd6fda1da0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb9ef788daca13ff85e7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bbe8b6c443face5f8c90",
+    "url": "/static/js/2.2aea2b18.chunk.js"
+  },
+  {
+    "revision": "bb9ef788daca13ff85e7",
+    "url": "/static/js/main.222bd675.chunk.js"
+  },
+  {
+    "revision": "531da51783bc97239777",
+    "url": "/static/js/runtime-main.37101f40.js"
+  },
+  {
+    "revision": "47fb27b47d55a4c3d89bad32a143891a",
+    "url": "/static/media/background.47fb27b4.jpg"
+  }
+]);
