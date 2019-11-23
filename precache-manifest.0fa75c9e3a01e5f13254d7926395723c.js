5b6589b9348fbfaff824e938aa7d4b93880adeaa
nedakinduryte <neda.kinduryte@gmail.com>
2019-11-23 18:00:00 +0000

    Updates

---
+++ precache-manifest.0fa75c9e3a01e5f13254d7926395723c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a896f1fb4f1ebbd79bcb7549fd01685c",
+    "url": "/countdown-app/index.html"
+  },
+  {
+    "revision": "812ed8e85b049dce353c",
+    "url": "/countdown-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bbe8b6c443face5f8c90",
+    "url": "/countdown-app/static/js/2.2aea2b18.chunk.js"
+  },
+  {
+    "revision": "812ed8e85b049dce353c",
+    "url": "/countdown-app/static/js/main.568d9341.chunk.js"
+  },
+  {
+    "revision": "edb82cd8fae608a54739",
+    "url": "/countdown-app/static/js/runtime-main.1fc0b187.js"
+  },
+  {
+    "revision": "47fb27b47d55a4c3d89bad32a143891a",
+    "url": "/countdown-app/static/media/background.47fb27b4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b1a3720dcf4062f5a08cb1cf0e9b24b.js"
+  "/countdown-app/precache-manifest.0fa75c9e3a01e5f13254d7926395723c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/countdown-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
